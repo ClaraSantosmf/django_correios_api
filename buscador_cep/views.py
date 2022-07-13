796a3fa41a0b79f844ba4542224c90d1def6f4fd
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-07-13 13:45:07 -0300

    redirected the project (core) url to app (buscador_cep) url

--- buscador_cep/views.py
+++ buscador_cep/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+# from .models import ConsultarCep
+
 
 # Create your views here.
+
+
+def index(request):
+    cep = 'todo'
+    cidade_estado = 'todo'
+    bairro = 'todo'
+    complemento = 'todo'
+
+    context = {
+        'cep': cep,
+        'cidade_estado': cidade_estado,
+        'bairro': bairro,
+        'complemento': complemento}
+    
+    return render(request, 'index.html', context)
