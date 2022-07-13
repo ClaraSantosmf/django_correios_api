63cd40fb6cc5ee62a7057e9b460233bc5bd83529
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-07-13 12:40:57 -0300

    started to think in models project, added the static file with the ceps as read-only

--- buscador_cep/models.py
+++ buscador_cep/models.py
@@ -1,50 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 
-
-class Estados():
-    '''
-    estados:
-    1,Acre,AC
-    2,Alagoas,AL
-    3,Amazonas,AM
-    campos:
-    id
-    nome
-    sigla
-    '''
-    pass
-
-
-class Cidades():
-    '''
-    cidades:
-    1,Abacate da Pedreira (Macapá),4
-    2,Abadia de Goiás,9
-    3,Abadia dos Dourados,11
-
-    campos:
-    id
-    nome
-    id_estado
-    '''
-    pass
-
+'''
+cep         cidade/estado   bairro/distrito     rua                                 complemento (pode ser nulo)
+01005010    São Paulo/SP    Sé                  Largo São Francisco
+01005020    São Paulo/SP    Sé                  Rua São Francisco
+01005030    São Paulo/SP    Sé                  Rua do Ouvidor
+01005900    São Paulo/SP    Sé                  Rua Benjamin Constant, 153          Edifício Sinhara
+01006000    São Paulo/SP    Sé                  Rua Senador Feijó - lado par
+01006001    São Paulo/SP    Sé                  Rua Senador Feijó - lado ímpar
+'''
 
 class Ceps():
-    '''
-    ceps:
-    01001000, Praça da Sé, - lado ímpar, Sé, 8966, 26
-    01001001, Praça da Sé, - lado par, Sé, 8966, 26
-    01001010, Rua Filipe de Oliveira, , Sé, 8966, 26
-
-    campos:
-    id(cep)
-    logradouro/nome
-    complemento(deve ser juntado com o campo anterior)
-    bairro/distrito
-    id_cidade
-    id_estado
-    '''
-    pass
+    cep = models.PositiveIntegerField()
+    cidade_estado = models.CharField(max_length=48)
+    bairro = models.CharField(max_length=64)
