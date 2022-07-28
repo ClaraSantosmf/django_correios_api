de57600d9cf9d8c96508acc668107aef9dadcff6
ClaraSantosmf <clarasantosmf@gmail.com>
2022-07-28 15:41:36 -0300

    tests setup

--- buscador_cep/tests.py
+++ buscador_cep/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+def test_index(client, db):
+    resposta = client.get(reverse('index'))
+    assert resposta.status_code == 200
+
